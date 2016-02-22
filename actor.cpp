996d723e82d525ad9e4f5c37e5213700957258fd
seanlangley <sean.langley22@gmail.com>
2016-02-21 17:13:25 -0800

    Update actor.cpp

--- actor.cpp
+++ actor.cpp
@@ -4,35 +4,18 @@
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
 
-////////////////////////////////////////////////////////////////////////////////
-// Actor Implementation
-////////////////////////////////////////////////////////////////////////////////
 
-Actor::Actor(int IID, int x, #include "Actor.h"
-#include "StudentWorld.h"
-
-
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
-
-const int BOULDER = 0;
-const int SQUIRT = 1;
-const int BARREL_OF_OIL = 2;
-const int GOLD_NUGGET = 3;
-const int SONAR_KIT = 4;
-const int WATER_POOL = 5;
-const int REGULAR_PROTESTOR = 6;
-const int HARDCORE_PROTESTOR = 7;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Actor Implementation
 ////////////////////////////////////////////////////////////////////////////////
 
-Actor::Actor(int IID, int x, int y, Direction dir, bool isVisible, float size, unsigned  int depth,  StudentWorld* sw)
-:GraphObject(IID, x,y, dir, size, depth), m_world(sw)
+Actor::Actor(int IID, int x, int y, Direction dir, float size, unsigned  int depth,  StudentWorld* sw)
+:GraphObject(IID, x,y, dir, size, depth), m_world(sw), m_dead(false)
 {
     if(sw == nullptr)
         exit(1);
-    setVisible(isVisible);
+    setVisible(true);
 }
 
 Actor::~Actor()
@@ -46,7 +29,7 @@ Actor::~Actor()
 ////////////////////////////////////////////////////////////////////////////////
 
 Dirt::Dirt(int x, int y, StudentWorld* sw)
-:Actor(IID_DIRT, x,y, right, true, .25, 3,  sw)
+:Actor(IID_DIRT, x,y, right, .25, 3,  sw)
 {}
 
 Dirt::~Dirt(){return;}
@@ -58,12 +41,16 @@ Dirt::~Dirt(){return;}
 ////////////////////////////////////////////////////////////////////////////////
 
 FrackMan::FrackMan(StudentWorld* sw)
-:Actor(IID_PLAYER, 30,60, right, true, 1.0, 0, sw), m_health(100)
+:Actor(IID_PLAYER, 30,60, right, 1.0, 0, sw), m_health(100), m_score(0)
 {}
 
 FrackMan::~FrackMan(){return;}
 
-
+void FrackMan::setDead()
+{
+    m_health = 0;
+    getWorld()->playSound(SOUND_PLAYER_GIVE_UP);
+}
 
 void FrackMan::doSomething()
 {
@@ -88,8 +75,7 @@ void FrackMan::move(Direction dir)
     switch(dir)
     {
         case left:
-            if(getWorld()->isThereActor(getX()-1, getY())
-               || getWorld()->isThereActor(getX()-1, getY()+3))
+            if(getWorld()->isThereBoulder(getX()-1, getY()) || getWorld()->isThereBoulder(getX()-1, getY()+3))
                 break;
             setDirection(left);
             if(getX() > 0)
@@ -98,7 +84,7 @@ void FrackMan::move(Direction dir)
                 moveTo(getX(), getY());
             break;
         case right:
-            if(getWorld()->isThereActor(getX()+4, getY()) || getWorld()->isThereActor(getX()+4, getY()+3))
+            if(getWorld()->isThereBoulder(getX()+4, getY()) || getWorld()->isThereBoulder(getX()+4, getY()+3))
                 break;
             setDirection(right);
             if(getX() < 60)
@@ -107,7 +93,7 @@ void FrackMan::move(Direction dir)
                 moveTo(getX(), getY());
             break;
         case down:
-            if(getWorld()->isThereActor(getX(), getY()-1) || getWorld()->isThereActor(getX()+3, getY()-1))
+            if(getWorld()->isThereBoulder(getX(), getY()-1) || getWorld()->isThereBoulder(getX()+3, getY()-1))
                 break;
             setDirection(down);
             if(getY() > 0)
@@ -116,7 +102,7 @@ void FrackMan::move(Direction dir)
                 moveTo(getX(), getY());
             break;
         case up:
-            if(getWorld()->isThereActor(getX(), getY()+4) || getWorld()->isThereActor(getX()+3, getY()+4))
+            if(getWorld()->isThereBoulder(getX(), getY()+4) || getWorld()->isThereBoulder(getX()+3, getY()+4))
                 break;
             setDirection(up);
             if(getY() < 60)
@@ -133,7 +119,9 @@ void FrackMan::move(Direction dir)
 void FrackMan::react(int Actor)
 {
     if(Actor == BOULDER)
+    {
         setDead();
+    }
     if(Actor == SQUIRT)
         return;
     if(Actor == BARREL_OF_OIL)
@@ -150,14 +138,23 @@ void FrackMan::react(int Actor)
         return;
 }
 
-
+bool FrackMan::isWithinRadius(Actor* actor)
+{
+    int x = actor->getX();
+    int y = actor->getY();
+    for(int k = 0; k < 4; k++)
+        for(int i = 0; i < 4; i++)
+            if((x+4+k == getX() && x+4+y == getY()) || (x-k == getX() && y-i == getY()))
+                return true;
+    return false;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Boulder Implementation
 ////////////////////////////////////////////////////////////////////////////////
 
 Boulder::Boulder(int x, int y, StudentWorld* sw)
-:Actor(IID_BOULDER, x,y, down, true, 1.0, 1, sw), m_state(STABLE)
+:Actor(IID_BOULDER, x,y, down, 1.0, 1, sw), m_state(STABLE), m_count(0)
 {}
 
 void Boulder::doSomething()
@@ -167,20 +164,36 @@ void Boulder::doSomething()
     int x = getX(), y = getY();
     if(getWorld()->isThereDirt(x, y-1))
         return;
-    while(! getWorld()->isThereDirt(getX(), getY()-1))
-          moveTo(getX(), getY()-1);
-            if(getWorld()->getPlayer()->getX() == getX() && getWorld()->getPlayer()->getY() == getY())
-                getWorld()->getPlayer()->react(BOULDER);
+    
+    if(! getWorld()->isThereDirt(getX(), getY()-1))
+       {
+           if(m_count < 29)
+           {
+               m_count++;
+               return;
+           }
            
-    
-    
+           else if(m_count == 29)
+           {
+               getWorld()->playSound(SOUND_FALLING_ROCK);
+               m_count++;
+               
+           }
+           else if(m_count > 29)
+           {
+               moveTo(getX(), getY()-1);
+           }
+           
+       }
+    setDead();
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Squirt Implementation
 ////////////////////////////////////////////////////////////////////////////////
 Squirt::Squirt(int x, int y, Direction dir, StudentWorld* sw)
-:Actor(IID_WATER_SPURT, x,y, dir, true ,1.0, 1, sw)
+:Actor(IID_WATER_SPURT, x,y, dir, 1.0, 1, sw)
 {
     
 }
@@ -189,194 +202,32 @@ Squirt::Squirt(int x, int y, Direction dir, StudentWorld* sw)
 // Barrel of Oil Implementation
 ////////////////////////////////////////////////////////////////////////////////
 BarrelOfOil::BarrelOfOil(int x, int y, StudentWorld* sw)
-:Actor(IID_BARREL, x,y, right, false, 1.0, 2, sw)
-{
-    
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Gold Nugget Implementation
-////////////////////////////////////////////////////////////////////////////////
-GoldNugget::GoldNugget(int x, int y, bool isVisible, StudentWorld* sw)
-:Actor(IID_GOLD, x,y, right, isVisible, 1.0, 2, sw)
-{
-    
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Sonar Kit Implementation
-////////////////////////////////////////////////////////////////////////////////
-SonarKit::SonarKit(int x, int y, StudentWorld* sw)
-:Actor(IID_SONAR, x,y, right, true, 1.0, 2, sw)
-{
-    
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Water Pool Implementation
-////////////////////////////////////////////////////////////////////////////////
-WaterPool::WaterPool(int x, int y, StudentWorld* sw)
-:Actor(IID_WATER_POOL, x,y, right, true, 1.0, 2, sw)
-{
-    
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Regular Protestor Implementation
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Hardcore Protestor Implementation
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int y, Direction dir, bool isVisible, float size, unsigned  int depth,  StudentWorld* sw)
-:GraphObject(IID, x,y, dir, size, depth), m_world(sw)
-{
-    if(sw == nullptr)
-        exit(1);
-    setVisible(isVisible);
-}
-
-Actor::~Actor()
-{return;
-//    setVisible(false);
-    
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Dirt Implementation
-////////////////////////////////////////////////////////////////////////////////
-
-Dirt::Dirt(int x, int y, StudentWorld* sw)
-:Actor(IID_DIRT, x,y, right, true, .25, 3,  sw)
+:Actor(IID_BARREL, x,y, right, 1.0, 2, sw)
 {}
 
-Dirt::~Dirt(){return;}
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// FrackMan Implementation
-////////////////////////////////////////////////////////////////////////////////
-
-FrackMan::FrackMan(StudentWorld* sw)
-:Actor(IID_PLAYER, 30,60, right, true, 1.0, 0, sw), m_health(10)
-{}
-
-FrackMan::~FrackMan(){return;}
-
-
-void FrackMan::doSomething()
+void BarrelOfOil::doSomething()
 {
-    int ch;
-    if(getWorld()->getKey(ch) == true)
+    if(isDead())
+        return;
+    if(isVisible() == true && getWorld()->getPlayer()->isWithinRadius(this))
     {
-        switch(ch)
-        {
-            case KEY_PRESS_LEFT:
-                setDirection(left);
-                if(getX() > 0)
-                    moveTo(getX()-1, getY());
-                else
-                    moveTo(getX(), getY());
-                break;
-            case KEY_PRESS_RIGHT:
-                setDirection(right);
-                if(getX() < 60)
-                    moveTo(getX()+1, getY());
-                else
-                    moveTo(getX(), getY());
-                break;
-            case KEY_PRESS_DOWN:
-                setDirection(down);
-                if(getY() > 0)
-                    moveTo(getX(), getY()-1);
-                else
-                    moveTo(getX(), getY());
-                break;
-            case KEY_PRESS_UP:
-                setDirection(up);
-                if(getY() < 60)
-                    moveTo(getX(), getY()+1);
-                else
-                    moveTo(getX(), getY());
-                break;
-                
-        }
+        getWorld()->playSound(SOUND_FOUND_OIL);
+        getWorld()->getPlayer()->increaseScore(1000);
+        getWorld()->gotBarrel();
+        setDead();
     }
 }
 
-
-
-
-
-
-void FrackMan::getAnnoyed(){return;}
-
-////////////////////////////////////////////////////////////////////////////////
-// Boulder Implementation
-////////////////////////////////////////////////////////////////////////////////
-
-Boulder::Boulder(int x, int y, StudentWorld* sw)
-:Actor(IID_BOULDER, x,y, down, true, 1.0, 1, sw), m_isAlive(true)
-{}
-
-void Boulder::doSomething()
-{
-    if(! m_isAlive)
-        return;
-    
-    
-    
-    
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Squirt Implementation
-////////////////////////////////////////////////////////////////////////////////
-Squirt::Squirt(int x, int y, Direction dir, StudentWorld* sw)
-:Actor(IID_WATER_SPURT, x,y, dir, true ,1.0, 1, sw)
-{
-    
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Barrel of Oil Implementation
-////////////////////////////////////////////////////////////////////////////////
-BarrelOfOil::BarrelOfOil(int x, int y, StudentWorld* sw)
-:Actor(IID_BARREL, x,y, right, false, 1.0, 2, sw)
-{
-    
-}
+void BarrelOfOil::getAnnoyed(){return;}
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Gold Nugget Implementation
 ////////////////////////////////////////////////////////////////////////////////
 GoldNugget::GoldNugget(int x, int y, bool isVisible, StudentWorld* sw)
-:Actor(IID_GOLD, x,y, right, isVisible, 1.0, 2, sw)
+:Actor(IID_GOLD, x,y, right, 1.0, 2, sw)
 {
-    
+    setVisible(isVisible);
 }
 
 
@@ -384,7 +235,7 @@ GoldNugget::GoldNugget(int x, int y, bool isVisible, StudentWorld* sw)
 // Sonar Kit Implementation
 ////////////////////////////////////////////////////////////////////////////////
 SonarKit::SonarKit(int x, int y, StudentWorld* sw)
-:Actor(IID_SONAR, x,y, right, true, 1.0, 2, sw)
+:Actor(IID_SONAR, x,y, right, 1.0, 2, sw)
 {
     
 }
@@ -394,7 +245,7 @@ SonarKit::SonarKit(int x, int y, StudentWorld* sw)
 // Water Pool Implementation
 ////////////////////////////////////////////////////////////////////////////////
 WaterPool::WaterPool(int x, int y, StudentWorld* sw)
-:Actor(IID_WATER_POOL, x,y, right, true, 1.0, 2, sw)
+:Actor(IID_WATER_POOL, x,y, right, 1.0, 2, sw)
 {
     
 }
@@ -403,13 +254,17 @@ WaterPool::WaterPool(int x, int y, StudentWorld* sw)
 ////////////////////////////////////////////////////////////////////////////////
 // Regular Protestor Implementation
 ////////////////////////////////////////////////////////////////////////////////
-
+RegularProtestor::RegularProtestor(int x, int y, StudentWorld* sw)
+:Actor(IID_PROTESTER, x,y, left, 1.0, 0, sw)
+{}
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Hardcore Protestor Implementation
 ////////////////////////////////////////////////////////////////////////////////
-
+HardcoreProtestor::HardcoreProtestor(int x, int y, StudentWorld* sw)
+:RegularProtestor(x,y, sw)
+{}
 
 
 
